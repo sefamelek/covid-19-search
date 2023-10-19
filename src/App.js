90e8375accd92328b7345858bacc26ba65a08d69
sefamelek <sefa.melek@hotmail.com>
2023-10-19 23:24:44 +0300

    sefa

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{useEffect,useState} from 'react'
+import axios from 'axios'
 function App() {
+  const [veri,setVeri]=useState("");
+  const [tarih,setTarih]=useState("");
+
+  useEffect(()=>{
+
+    axios.get("https://raw.githubusercontent.com/ozanerturk/covid19-turkey-api/master/dataset/timeline.json")
+    .then(res=>setVeri(res.data[tarih]))
+    .catch(err=>console.log(err))
+  },[veri,tarih])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 mx-auto mt-4">
+            <h2 className="text-center text-white display-3">Türkiye Covid-19 Arama Motoru</h2>
+            <input type="text" placeholder="GG/AA/YYYY"  className="form-control" onChange={(e)=>setTarih(e.target.value)}/>
+            <table className="table table-striped table-dark table-hover">
+              <thead>
+                <tr className="text-white">
+                  <th scope="col">#</th>
+                  <th scope="col">Test Sayısı</th>
+                  <th scope="col">Hasta Sayısı</th>
+                  <th scope="col">Vefat Sayısı</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className='text-white'className={veri===undefined? "table-danger" : "table-success"} >
+                  <th scope="row">{veri===undefined? "Veri Bekleniyor" : veri.date}</th>
+                  <td >{veri===undefined? "Veri Bekleniyor" : veri.tests}
+            </td>
+                  <td>{veri===undefined? "Veri Bekleniyor" : veri.patients}</td>
+                  <td>{veri===undefined? "Veri Bekleniyor" : veri.deaths}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        
+      </div>
+      
     </div>
   );
 }
